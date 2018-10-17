5583658f1062a40fd098bd9aa811eca7fca774de
Togami Yuki <togaminnnn@gmail.com>
2018-10-17 15:58:23 +0800

    Firestoreへデータを追加する処理を追加。

--- FirestoreSampleToDo/ViewController.swift
+++ FirestoreSampleToDo/ViewController.swift
@@ -7,19 +7,90 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var todoTextField: UITextField!
+    @IBOutlet weak var todoTableView: UITableView!
+    
+    //Firestoreを扱うための変数を宣言。
+    var db : Firestore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        todoTableView.delegate = self
+        todoTableView.dataSource = self
+        
+        //Firestoreのインスタンス化
+        db = Firestore.firestore()
+        
     }
-
+    
+    //Firestoreへのデータの追加
+    @IBAction func create(_ sender: UIButton) {
+        var data = ["todo":todoTextField.text!]
+        creatData(data:data)
+    }
+    
+    //Firestoreからのデータの読み込み
+    @IBAction func read(_ sender: UIButton) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
+}
+
+//Firestoreデータベースの処理の関する関数。
+extension ViewController{
+    //データ追加
+    func creatData(data:[String:Any]){
+        db.collection("ToDoList").addDocument(data: data){ err in
+            if let err = err {
+                print("Error writing document: \(err)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
+    }
+    //データ読み込み
+    func readData(){
+        
+    }
+    //データ更新
+    func update(){
+        
+    }
+    //データ削除
+    func delete(){
+        
+    }
+}
 
 
+
+
+
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    
+    //セルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    //セルのインスタンス化
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "あ"
+        
+        return cell
+    }
+    
+    
 }
 
